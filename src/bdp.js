c32414cc4b0034ea9b7397d21c120f08421f3eea
Sergey Romanov <xxsmotur@gmail.com>
2015-08-04 23:51:44 +0500

    first commit

---
+++ src/bdp.js
@@ -0,0 +1,54 @@
+
+//https://en.wikipedia.org/wiki/Birth%E2%80%93death_process
+//
+export class BDP {
+    constructor(lambdas, mus) {
+        if(lambdas.length !== mus.length){
+            throw "Length of arrays is not equal";
+        }
+
+        this.lambdas = lambdas;
+        this.mus = mus;
+    }
+
+    //Return from the first state at the time t
+    probability(t) {
+        let start = start_probability(this.lambdas, this.mus);
+        if(t === 0){
+            return start;
+        }
+
+        if(t > this.lambdas.length+1){
+            throw "t is greather then lambdas length";
+        }
+
+        let result = 0;
+        let last_probability = start;
+        for(let i = 0;i < t;++i) {
+            let item = 1;
+            for(let j = 0;j <= i;++j) {
+                let tmp = (this.lambdas[j])/this.mus[j];
+                item *= tmp;
+            }
+            last_probability*=item;
+        }
+
+        return last_probability;
+    }
+}
+
+let start_probability = function(lambdas, mus){
+     let result = 0.0;
+     for(let i = 0;i < lambdas.length;++i)
+     {
+        let item = 1.0;
+        for(let j = 0;j <= i;++j){
+            let tmp = lambdas[j]/mus[j];
+            item *= tmp;
+        }
+        result += item;
+
+    }
+    return 1/(1 + result);
+
+}
