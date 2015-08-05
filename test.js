4fd0a5f552ba80ce232e3ce889d7c0b186c243fb
Sergey Romanov <xxsmotur@gmail.com>
2015-08-05 22:52:12 +0500

    Add test

---
+++ test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var bdp = require('./lib/bdp');
+
+it('should return probability at the state n', function(){
+    var lambda = [5,4,2];
+    var mu = [4,5,10];
+    var bdpmodel = new bdp.BDP(lambda, mu);
+    assert.equal(bdpmodel.probability(0), 0.2898550724637681);
+    assert.equal(bdpmodel.probability(1), 0.36231884057971014);
+});
